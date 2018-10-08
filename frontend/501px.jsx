c3b730bd6cae725970bbb1854de4a2fe0429c2d9
Devin Flood <devin.p.flood@gmail.com>
2018-10-07 17:09:53 -0700

    Node & Webpack setup

---
+++ frontend/501px.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  // window.signup = signup;
+  // window.login = login;
+  // window.logout = logout;
+  // const store = configureStore();
+  // window.store = store;
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Welcome!</h1>, root);
+});
