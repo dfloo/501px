d7cfb585b7b57ecb7e0d429d5f5a83620ff2bdc1
Devin Flood <devin.p.flood@gmail.com>
2018-10-09 16:17:18 -0700

    Complete users migration and users, session & application controllers

--- frontend/501px.jsx
+++ frontend/501px.jsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  // window.signup = signup;
-  // window.login = login;
-  // window.logout = logout;
-  // const store = configureStore();
-  // window.store = store;
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome!</h1>, root);
+  ReactDOM.render(<h1>Welcome to 501px!</h1>, root);
 });
